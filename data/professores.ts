6b638e6c85a108d3600e4fbe49abfa0c43b61413
ESFERA <esferaaulasparticulares@gmail.com>
2026-03-15 13:08:19 -0300

    atualiza professores reais do ESFERA

--- data/professores.ts
+++ data/professores.ts
@@ -1,22 +1,16 @@
-import { Professor } from "@/lib/types";
-
 export const professores: Professor[] = [
   {
     id: "p1",
-    nome: "Prof. Lucas Andrade",
-    area: "Matemática e Cálculo",
-    descricao: "Atuação em reforço escolar e apoio acadêmico para graduação em áreas exatas."
+    nome: "Prof. Alex",
+    area: "Matemática",
+    descricao:
+      "Estudou na Universidade de São Paulo (USP) e na Universidade Estadual de Campinas (UNICAMP), possui mais de 20 anos de experiência no ensino de Matemática em cursos pré-vestibulares. Atua com aulas para ensino médio, preparação para vestibulares e também para ensino superior, incluindo disciplinas como Cálculo, Geometria Analítica e Vetores, Álgebra Linear, ..."
   },
   {
     id: "p2",
-    nome: "Profa. Mariana Lopes",
-    area: "Língua Portuguesa",
-    descricao: "Acompanhamento em leitura, escrita e preparação para avaliações escolares."
-  },
-  {
-    id: "p3",
-    nome: "Prof. Henrique Souza",
-    area: "Física e Estatística",
-    descricao: "Apoio para ensino médio e disciplinas quantitativas no ensino superior."
+    nome: "Nicolas",
+    area: "Física",
+    descricao:
+      "Doutorando em Física pela Universidade Estadual Paulista (UNESP), possui mais de 5 anos de experiência em aulas particulares. Atua com alunos do ensino fundamental, ensino médio, preparação para vestibulares e também no ensino superior."
   }
 ];
